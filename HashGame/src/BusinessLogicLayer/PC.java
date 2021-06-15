4fb3415cd88e9a6ce207977500ce2a03505c5aad
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2021-06-15 20:02:58 -0300

    -PC implementado;

--- HashGame/src/BusinessLogicLayer/PC.java
+++ HashGame/src/BusinessLogicLayer/PC.java
@@ -1,5 +1,19 @@
 package BusinessLogicLayer;
 
 public class PC {
-    
+    private char Letra;
+    private Mapa Mapa;
+
+    public PC(Mapa mapa) {
+        Letra = 'O';
+        Mapa = mapa;
+    }
+
+    public boolean Jogar(){
+        int linhaSorteada = Mapa.Sortear(0, 2);
+        int colunaSorteada = Mapa.Sortear(0, 2);
+        
+        Mapa.Jogar(linhaSorteada, colunaSorteada, Letra);
+        return Mapa.VerificarGanhador(Letra);
+    }
 }
