2e6005f2d1c7865e228ac257f4fb2ab6591fd9a5
Gabriel Voltolini <gabrielarnovolyolini1945@gmail.com>
2021-06-15 19:09:35 -0300

    -Mapa revisado e finalizado;

--- HashGame/src/BusinessLogicLayer/Mapa.java
+++ HashGame/src/BusinessLogicLayer/Mapa.java
@@ -1,20 +1,6 @@
 package BusinessLogicLayer;
 
 public class Mapa {
-    /*
-     * mapa é definido pelo tipo primitivo char OK
-     * 
-     * implementa o método sortear disponível em Exemplos; OK
-     * 
-     * usar o carácter ' ' (espaço em branco) para limpar o mapa; Ok
-     * 
-     * a cada jogada testar quem venceu a partida (jogador ou PC); considerar o caso OK
-     * de empate no final da partida;
-     * 
-     * testar no método jogar se a linha e coluna recebida representa uma célula OK
-     * livre no mapa, e em caso positivo marcar nesta célula quem jogou (usar a
-     * letra).
-     */
 
     private char[][] _mapa;
 
@@ -51,26 +37,26 @@ public class Mapa {
         c--;
 
         boolean jogadaValida = false;
-        if ((l <= 3 && l >= 0) && (c <= 3 && c >= 0)) {
+        if ((l <= 2 && l >= 0) && (c <= 2 && c >= 0)) {
             if (_mapa[l][c] == ' ') {
                 jogadaValida = true;
             }
         }
-        
+
         if (jogadaValida)
             _mapa[l][c] = jogador;
     }
 
     public boolean VerificarGanhador(char jogador) {
-        if (_mapa[0][0] == jogador && _mapa[1][1] == jogador && _mapa[2][2] == jogador   // diagonal 0
-        ^   _mapa[2][2] == jogador && _mapa[1][1] == jogador && _mapa[2][0] == jogador   // diagonal 2
-        ^   _mapa[0][0] == jogador && _mapa[0][1] == jogador && _mapa[0][2] == jogador   // reta 1
-        ^   _mapa[1][0] == jogador && _mapa[1][1] == jogador && _mapa[1][2] == jogador   // reta 2
-        ^   _mapa[2][0] == jogador && _mapa[2][1] == jogador && _mapa[2][2] == jogador   // reta 3
-        ^   _mapa[0][0] == jogador && _mapa[1][0] == jogador && _mapa[2][0] == jogador   // reta 4
-        ^   _mapa[0][1] == jogador && _mapa[1][1] == jogador && _mapa[2][1] == jogador   // reta 5
-        ^   _mapa[0][2] == jogador && _mapa[1][2] == jogador && _mapa[2][2] == jogador   // reta 6
-        ){
+        if (_mapa[0][0] == jogador && _mapa[1][1] == jogador && _mapa[2][2] == jogador // diagonal 0
+                || _mapa[0][2] == jogador && _mapa[1][1] == jogador && _mapa[2][0] == jogador // diagonal 2
+                || _mapa[0][0] == jogador && _mapa[0][1] == jogador && _mapa[0][2] == jogador // reta 1
+                || _mapa[1][0] == jogador && _mapa[1][1] == jogador && _mapa[1][2] == jogador // reta 2
+                || _mapa[2][0] == jogador && _mapa[2][1] == jogador && _mapa[2][2] == jogador // reta 3
+                || _mapa[0][0] == jogador && _mapa[1][0] == jogador && _mapa[2][0] == jogador // reta 4
+                || _mapa[0][1] == jogador && _mapa[1][1] == jogador && _mapa[2][1] == jogador // reta 5
+                || _mapa[0][2] == jogador && _mapa[1][2] == jogador && _mapa[2][2] == jogador // reta 6
+        ) {
             return true;
         }
         return false;
